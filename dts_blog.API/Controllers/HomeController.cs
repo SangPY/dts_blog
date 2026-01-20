6da16c602530298c9514488c54522e2fd3b9276e
sangdothanh95 <sangdothanh95@gmail.com>
2026-01-20 09:44:56 +0700

    Add project files.

---
+++ dts_blog.API/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace dts_blog.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        public IActionResult Index()
+        {
+            return Ok();
+        }
+    }
+}
---
+++ dts_blog.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace dts_blog.WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
