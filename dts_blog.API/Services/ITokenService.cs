5e079f056a771d526c697820b9bf0e7f79b561ca
sangdothanh95 <sangdothanh95@gmail.com>
2026-01-21 11:22:39 +0700

    Authentication and authorization

---
+++ dts_blog.API/Services/ITokenService.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace dts_blog.API.Services
+{
+    public interface ITokenService
+    {
+        string GenerateAccessToken(IEnumerable<Claim> claims);
+        string GenerateRefreshToken();
+        ClaimsPrincipal GetPrincipalFromExpiredToken(string token);
+    }
+}
---
+++ dts_blog.Core/Models/Auth/LoginRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace dts_blog.Core.Models.Auth
+{
+    public class LoginRequest
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+    }
+}
